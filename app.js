b0e85f84da23123f3562d4339ae088fe3b8dff1a
rahmatbrylyant975@gmail.com <rahmatbrylyant975@gmail.com>
2026-07-08 23:33:47 +0700

    array of objects for menu card

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const increaseQty = document.querySelectorAll(".add-item");
 const decreaseQty = document.querySelectorAll(".remove-item");
 const displayQty = document.querySelectorAll(".item-qty");
+const menuCard = document.querySelectorAll(".menu-card");
+const submitBtn = document.querySelector("#submit-btn");
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const orderMenu = [];
+
+  menuCard.forEach((card) => {
+    let name = card.dataset.name;
+    let price = card.dataset.price;
+    let qty = card.querySelector(".item-qty").innerText;
+    if (parseInt(qty) > 0) {
+      orderMenu.push({ name, price, qty });
+    }
+  });
+  console.log(orderMenu);
+});
 
 increaseQty.forEach((btn, index) => {
   btn.addEventListener("click", (e) => {
