a301ce0963bc576960e1a43a59d7908052c05d24
rahmatbrylyant975@gmail.com <rahmatbrylyant975@gmail.com>
2026-07-10 23:51:46 +0700

    renaming and create container for order summary

--- app.js
+++ app.js
@@ -1,39 +1,38 @@
 const increaseQty = document.querySelectorAll(".add-item");
 const decreaseQty = document.querySelectorAll(".remove-item");
-const displayQty = document.querySelectorAll(".item-qty");
+const itemQty = document.querySelectorAll(".item-qty");
 const menuCard = document.querySelectorAll(".menu-card");
 const submitBtn = document.querySelector("#submit-btn");
-const name = document.querySelector("#name").value;
-const number = document.querySelector("#number").value;
-const address = document.querySelector("#address").value;
-const date = document.querySelector("#date").value;
-const note = document.querySelector("#note").value;
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
+  const name = document.querySelector("#name").value;
+  const number = document.querySelector("#number").value;
+  const address = document.querySelector("#address").value;
+  const date = document.querySelector("#date").value;
+  const note = document.querySelector("#note").value;
   const orderMenu = [];
 
   menuCard.forEach((card) => {
-    let name = card.dataset.name;
-    let price = card.dataset.price;
-    let qty = card.querySelector(".item-qty").innerText;
+    let menuName = card.dataset.name;
+    let menuPrice = card.dataset.price;
+    let menuQty = card.querySelector(".item-qty").innerText;
     if (parseInt(qty) > 0) {
-      orderMenu.push({ name, price, qty });
+      orderMenu.push({ menuName, menuPrice, menuQty });
     }
   });
-  const menuList = orderMenu.map((item) => `-${item.name} x${item.qty}`);
 });
 
 increaseQty.forEach((btn, index) => {
   btn.addEventListener("click", (e) => {
-    displayQty[index].innerText = parseInt(displayQty[index].innerText) + 1;
+    itemQty[index].innerText = parseInt(itemQty[index].innerText) + 1;
   });
 });
 
 decreaseQty.forEach((btn, index) => {
   btn.addEventListener("click", (e) => {
-    if (parseInt(displayQty[index].innerText) !== 0) {
-      displayQty[index].innerText = parseInt(displayQty[index].innerText) - 1;
+    if (parseInt(itemQty[index].innerText) !== 0) {
+      itemQty[index].innerText = parseInt(itemQty[index].innerText) - 1;
     }
   });
 });
